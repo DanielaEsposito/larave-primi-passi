75b385b08a139994f06468ddf9f04f1a80c86f14
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-03-05 10:28:33 +0100

    add link and new homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+
+<body>
+    <h1>HOLA MONDO!</h1>
+    <ul>
+        <li><a href="">link</a></li>
+        <li><a href="{{ route('profile') }}">link2</a></li>
+        <li><a href="">link</a></li>
+    </ul>
+</body>
+
+</html>
